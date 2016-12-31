33207e5c87891f32b5ec03d6c8641ed68ed34d56
Thomas <came1788@gmail.com>
2016-12-31 11:51:21 -0700

    changed compiler flags to all -O3

--- src/buildIepoly1.sh
+++ src/buildIepoly1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gfortran environment.f90 util.f90 dslmpep_subroutines.f90 dgelmpep_subroutines.f90 dgeeam_subroutines.f90 ie_test_driver1.f90 -lscalapack-openmpi -llapack -lblas && rm *.mod
+gfortran -O3 environment.f90 util.f90 dslmpep_subroutines.f90 dgelmpep_subroutines.f90 dgeeam_subroutines.f90 ie_test_driver1.f90 -lscalapack-openmpi -llapack -lblas && rm *.mod
 

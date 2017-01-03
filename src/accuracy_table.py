867d11c6aa9a6f9cca2b955723e1f4c72c05535e
Nikolas I. Steckley <nsteckley314@gmail.com>
2017-01-02 18:33:08 -0800

    Completed python script for accuracy testing

--- src/accuracy_table.py
+++ src/accuracy_table.py
@@ -3,24 +3,19 @@ from numpy import *
 from pylab import *
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as pyplot
-problem, berr, ferr, data = [], [], [], []
+data = []
 
 with open('../results/outputAccuracy.csv') as f:
   reader = csv.reader(f)
   headers= next(reader)
   for row in reader:
-    problem.append(row[0].strip())
-    berr.append(row[1].strip())
-    ferr.append(row[2].strip())
+    data.append([row[0].strip(), row[1].strip(), row[2].strip()])
     
 fig=plt.figure()
 ax = fig.add_subplot(111)
 ax.xaxis.set_visible(False)
 ax.yaxis.set_visible(False)
 colLabels=("Problem", "BERR", "FERR")
-data = [problem, berr, ferr]
-the_table = ax.table(cellText=data,
-          colLabels=colLabels,
-          loc='center')
-plt.savefig("accuracy_table.png")
+the_table = ax.table(cellText=data, colLabels=colLabels, loc='center')
+plt.savefig("../results/accuracy_table.png")
 

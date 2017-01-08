26d528f3b37ea12543261b8a952946b4eadc3ef9
Thomas <came1788@gmail.com>
2017-01-08 01:23:16 -0700

    update

--- src/buildAccuracy.sh
+++ src/buildAccuracy.sh
@@ -15,5 +15,5 @@ do
     shift
 done
 
-gfortran $FLAGS environment.f90 util.f90 dslmpep_subroutines.f90 dgelmpep_subroutines.f90 dgeeam_subroutines.f90 accuracy_test_driver.f90 -lscalapack-openmpi -llapack -lblas && rm *.mod
+gfortran $FLAGS environment.f90 util.f90 dslmpep_subroutines.f90 dgelmpep_subroutines.f90 accuracy_test_driver.f90 -lscalapack-openmpi -llapack -lblas && rm *.mod
 mv a.out ../bin/accuracy.out
--- src/buildIepoly1.sh
+++ src/buildIepoly1.sh
@@ -15,5 +15,5 @@ do
     shift
 done
 
-gfortran $FLAGS environment.f90 util.f90 dslmpep_subroutines.f90 dgelmpep_subroutines.f90 dgeeam_subroutines.f90 ie_test_driver1.f90 -lscalapack-openmpi -llapack -lblas && rm *.mod
+gfortran $FLAGS environment.f90 util.f90 dslmpep_subroutines.f90 dgelmpep_subroutines.f90 ie_test_driver1.f90 -lscalapack-openmpi -llapack -lblas && rm *.mod
 mv a.out ../bin/iepoly1.out

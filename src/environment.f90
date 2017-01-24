b91119ff280a810eb618730554b3f8c10d36abed
Nikolas I. Steckley <nsteckley314@gmail.com>
2017-01-24 15:33:17 -0800

    Updated enviroment and removed eiscor

--- src/environment.f90
+++ src/environment.f90
@@ -1,8 +1,8 @@
 MODULE environment
 IMPLICIT NONE 
 CHARACTER(*), PARAMETER :: homeDir="/home/thomas/Documents/FORTRAN/Nick/LMPEPtests/"
-!CHARACTER(*), PARAMETER :: homeDir="/home/nsteckley/Documents/Personal/Cameron/LMPEP/tests/"
+!CHARACTER(*), PARAMETER :: homeDir="/home/nsteckley/Documents/Personal/Cameron/LMPEP/"
 CHARACTER(*), PARAMETER :: resultsDir=homeDir//"results/"
 CHARACTER(*), PARAMETER :: problemsDir=homeDir//"PROBLEMS/REAL/"
-
+CHARACTER(*), PARAMETER :: sampleProblemsDir=homeDir//"src/QEP3D/SampleData/"
 END MODULE environment

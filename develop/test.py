3334c4d4d36feee1d05e4ef5220b1599fc15419e
Nikolas Steckley <nsteckley314@gmail.com>
2017-08-08 12:24:25 -0700

    work on pyton test file. need to wrap fortran in C next

--- develop/test.py
+++ develop/test.py
@@ -1,22 +1,47 @@
 from ctypes import *
 from random import *
+import time
 import sys
-lib = cdll.LoadLibrary(“./libtest.a”)
+#Import Fortran library
+lib = cdll.LoadLibrary("/libtest.a")
 
+#Make methods available for execution
+dseval = lib.dseval
+drarr = lib.drarr
+drnum = lib.drnum
+
+zseval = lib.zseval
+zrarr = lib.zrarr
+zrnum = lib.zrnum
+
+#Command line arguements
 startDegree=sys.argv[1]
 maxDegree=sys.argv[2]
+dt=sys.argv[3]
 
+#Hard coded values
+itmax =100
+der = 2
+
+t, a = 0 #Need to allocate memory for this
 
 degree=startDegree
-while degree<maxDegree:
+if dt=="D":
+	while degree<maxDegree:
+		p = [None]* (degree+1)
+		start = time.clock
+		for i in range(itmax):
+			# NOTE: The byref() is necessary since
+			# FORTRAN does references,
+			# and not values (like e.g. C)
+			drarr(byref(p), byref(degree)+1)
+			drnum(byref(t))
+			dseval(byref(p), byref(t), byref(degree), byref(der), byref(a))
+		endTime = clock.time
+		print("Degree, Average Time")
+		print(degree + "," + (startTime-endTime)/itmax)
+		degree *= 2
 	
 
-	method = libadd.dseval
-	#
-	# The byref() is necessary since
-	# FORTRAN does references,
-	# and not values (like e.g. C)
-	#
-	method( byref(x), byref(y) )
-
-
+elif dt=="Z":
+	print(1)

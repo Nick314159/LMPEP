35de3bdcdaae117c8d9bcae6e91c1ae932fa720a
Nikolas Steckley <nsteckley314@gmail.com>
2017-08-10 11:52:02 -0700

    Doyxgen documentation for dseval

--- develop/dseval.f90
+++ develop/dseval.f90
@@ -1,32 +1,25 @@
-!************************************************************************
-!                           SUBROUTINE DSEVAL				            *
-!           Authors: Thomas R. Cameron, Nikolas I. Steckley             *
-!                           Date: 8/7/2017                              *
-!************************************************************************
-! Evaluates scalar polynomial with real coefficients at real number.    *
-!************************************************************************
-! Input Variables:                                                      *
-!   p: REAL(re8), array of dimension (d+1),                             *
-!       contains polynomial coefficients from constant to leading.      *
-!   t: REAL(re8), number that polynomial is evaluated at.               *
-!   deg: INTEGER(in4), degree of the polynomial                         *
-!   der: INTEGER(in1), derivative to be taken (0,1,2)                   *
-!                                                                       *
-! Output Variables:                                                     *
-!   a: REAL(re8), return value.                                         *
-!                                                                       *
-!   MEMORY: O(deg), FLOPS: O(deg)                                       *
+!>\author Thomas R. Cameron
+!>\author Nikolas I. Steckley
+!>\date 8/7/2017
+!>\brief Evaluates scalar polynomial with real coefficients at real number. 
+!>\param[in] p REAL(re8), array of dimension (d+1) contains polynomial coefficients from constant to leading. 
+!>\param[in] t REAL(re8), number that polynomial is evaluated at.
+!>\param[in] deg INTEGER(in4), degree of the polynomial.
+!>\param[in] der INTEGER(in1), derivative to be taken (0,1,2).
+!>\param[out] a REAL(re8), return value.    
+!>\note MEMORY: O(deg), FLOPS: O(deg)
 !************************************************************************
 SUBROUTINE dseval(p, t, deg, der, a)
 USE util
 IMPLICIT NONE
 !scalar arguments
-INTEGER(KIND=in1), INTENT(IN)   :: der
-INTEGER(KIND=in4), INTENT(IN)   :: deg
-REAL(KIND=re8), INTENT(IN)      :: t
-REAL(KIND=re8), INTENT(INOUT)   :: a
+INTEGER(KIND=in1), INTENT(IN)   :: der  
+INTEGER(KIND=in4), INTENT(IN)   :: deg 
+REAL(KIND=re8), INTENT(IN)      :: t 
+REAL(KIND=re8), INTENT(INOUT)   :: a          
+
 !array arguments
-REAL(KIND=re8), INTENT(IN)      :: p(*)
+REAL(KIND=re8), INTENT(IN)      :: p(*) 
 !local scalars
 INTEGER(KIND=in4)               :: k
 

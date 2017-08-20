1f8b9dd9cdf637a4eccd5f5000bd0f512438c31e
Thomas <thomas@localhost.localdomain>
2017-08-19 22:20:53 -0400

    update

--- develop/test/install.sh
+++ develop/test/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #compile scalar subroutines to object files
-gfortran -c -O3 balance.f90 DCFD.f90 DGR.f90 DRANDPOLYJT.f90 RESCHECK.f90 DAMVW.f90 DCFT.f90 DGTO2.f90 init_random_seed.f90 DCB.f90 DFCC.f90 DMQF.f90 DCDB.f90 DFGR.f90 DNORMALPOLY.f90 pzeros.f90
+gfortran -c -O3 balance.f90 DCFD.f90 DGR.f90 DRANDPOLYJT.f90 RESCHECK.f90 DAMVW.f90 DCFT.f90 DGTO2.f90 DCB.f90 DFCC.f90 DMQF.f90 DCDB.f90 DFGR.f90 DNORMALPOLY.f90 pzeros.f90
 
 #add object files to library and copy library to /usr/lib64
 ar crv libsptest.a *.o

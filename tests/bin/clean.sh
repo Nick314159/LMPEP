bfbda9032803a7e66983fc008510392523260d31
Thomas <thomas@localhost.localdomain>
2017-08-26 10:38:16 -0400

    removed unecessary files

--- tests/bin/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Removing old executables..."
-rm ./*.out
-
-echo "Removing old results..."
-rm ../results/*
-
-echo "Done with clean up!"
--- tests/src/AMVW/doubleshift/configure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dir=`pwd`
-str="HOMEDIR := $dir"
-sed -i "/HOMEDIR := /c $str" environment
-
-str="CHARACTER(*), PARAMETER :: resultsDir=\"$1/\""
-sed -i "/resultsDir=/c $str" tests/testDAMVW.f95

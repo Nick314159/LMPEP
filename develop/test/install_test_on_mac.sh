2612bda09773c9181e234c6d8657fee40573ae8f
Nikolas Steckley <nsteckley314@gmail.com>
2017-08-31 11:48:14 -0700

    created test on mac script

---
+++ develop/test/install_test_on_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#compile random generator subroutines
+gfortran -c -O3 ../init_random_seed.f90 ../daruv.f90
+
+#compile Bini subroutines
+gfortran -c -O3 ../cmerge.f90 ../cnvex.f90 ../ctest.f90 ../left.f90 ../right.f90
+
+#compile scalar LMPEP subroutines
+gfortran -c -O3   ../dzmod.f90 ../dsstart.f90 ../dseval.f90 ../drevseval.f90 ../dzseval.f90 ../dzrevseval.f90 ../dslcorr.f90 ../dzslcorr.f90 ../dslm.f90
+
+#add object files to library and copy library to /usr/lib64
+ar crv liblmpep.a *.o
+sudo cp liblmpep.a /usr/lib64/
+
+#compile test program to executable file
+gfortran -O3 test_on_mac.f90 -llmpep -o test_on_mac.exe
+
+#clean up
+rm *.o
+rm *.a
+#Generate documentation
+#doxygen Doxygen/config

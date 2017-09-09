70d2a9c1c6f5f38fb539879a1b3fd257e7a22358
Nikolas Steckley <nsteckley314@gmail.com>
2017-09-08 18:51:08 -0700

    Added wilknonson polynomial

--- develop/test/poly_test.f90
+++ develop/test/poly_test.f90
@@ -72,6 +72,50 @@ exacteigs(3)  = complex(cos(35d0/40d0*pi),0d0)
 exacteigs(2)  = complex(cos(37d0/40d0*pi),0d0)
 exacteigs(1)  = complex(cos(39d0/40d0*pi),0d0)
 
+!Wilkonson Polynomial
+p(21)= 243290200817664D4
+p(20)= -875294803676160D4
+p(19)= 138037597536407D5
+p(18)= -128709312451510D5
+p(17)= 803781182264505D4
+p(16)= -359997951794761D4
+p(15)= 120664780378037D4
+p(14)= -311333643161391D3
+p(13)= 630308120992949D2
+p(12)= -101422998655115D2
+p(11)= 130753501054049D2
+p(10)= -135585182899530D0
+p(9) = 11310276995381D0
+p(8) = -756111184500D0
+p(7) = 40171771630D0
+p(6) = -1672280820D0
+p(5) = 53327946D0
+p(4) = -1256850D0
+p(3) = 20615D0
+p(2) = -210D0
+p(1) = 1D0
+exacteigs(20)  = 20
+exacteigs(19)  = 19
+exacteigs(18)  = 18
+exacteigs(17)  = 17
+exacteigs(16)  = 16
+exacteigs(15)  = 15
+exacteigs(14)  = 14
+exacteigs(13)  = 13
+exacteigs(12)  = 12
+exacteigs(11)  = 11
+exacteigs(10)  = 10
+exacteigs(9)  = 9
+exacteigs(8)  = 8
+exacteigs(7)  = 7
+exacteigs(6)  = 6
+exacteigs(5)  = 5
+exacteigs(4)  = 4
+exacteigs(3)  = 3
+exacteigs(2)  = 2
+exacteigs(1)  = 1
+
+
 
 
 !LMPEP
@@ -106,6 +150,7 @@ DO i=1,deg
     PRINT*, dzmod(dble(exacteigs(i))-dble(root(i)),dimag(exacteigs(i))-dimag(root(i)))
 ENDDO
 PRINT*, ''
+DEALLOCATE(poly, radius, root, err)
 
 !AMVW
 ALLOCATE(REIGS(deg),IEIGS(deg),ITS(deg), p2(deg))
@@ -121,6 +166,7 @@ DO i=1,deg
 ENDDO
 DEALLOCATE(p, p2, REIGS,IEIGS,ITS, berr)
 PRINT*, ''
+
 CONTAINS
 
 SUBROUTINE dsort(er, ei, n)

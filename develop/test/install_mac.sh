d72954331d3cf8aa73092dff58f3f686daecd856
Thomas Cameron <thomas@thomass-air.employeewireless.davidson.edu>
2017-09-14 21:33:52 -0400

    updated mac installer

--- develop/test/install_mac.sh
+++ develop/test/install_mac.sh
@@ -8,9 +8,9 @@ ar crv liblmpeptest.a *.o
 sudo cp liblmpeptest.a /usr/local/lib
 
 #compile test program to executable file
-gfortran -O3 test_dslm.f90 -llmpeptest -llmpep -o test_dslm.exe
-gfortran -O3 dslm_comparisons.f90 -llmpeptest -llmpep -o dslm_comparisons.exe
-gfortran -O3 poly_test.f90 -llmpeptest -llmpep -o poly_test.exe
+gfortran -O3 test_dslm.f90 -L/usr/local/lib -llmpeptest -llmpep -o test_dslm.exe
+gfortran -O3 dslm_comparisons.f90 -L/usr/local/lib -llmpeptest -llmpep -o dslm_comparisons.exe
+gfortran -O3 poly_test.f90 -L/usr/local/lib -llmpeptest -llmpep -o poly_test.exe
 
 #clean up
 rm *.mod

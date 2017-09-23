3f2fb30bfabab786841246ffd0a5917cc4d76055
Nick314159 <nsteckley314@gmail.com>
2017-09-23 16:58:41 -0700

    added dslm2

--- develop/research/modification_test/install.sh
+++ develop/research/modification_test/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #compile test program to executable file
-gfortran -O3 test.f90 dslm1.f90 dslcorr1.f90 dzslcorr1.f90 dsam.f90 dsacorr.f90 dzsacorr.f90 -llmpeptest -llmpep -o test.exe
+gfortran -O3 test.f90 dslm1.f90 dslm2.f90 dslcorr1.f90 dzslcorr1.f90 dslcorr2.f90 dzslcorr2.f90 dsam.f90 dsacorr.f90 dzsacorr.f90 -llmpeptest -llmpep -o test.exe 
 
--- develop/research/modification_test/test.f90
+++ develop/research/modification_test/test.f90
@@ -34,8 +34,8 @@ READ(arg, '(I10)') maxDegree
 deg=startDegree
 itmax = 10
 OPEN(UNIT=1,FILE="results/results.csv")
-WRITE(1,'(A)') 'Degree, DSLM Time, DSLM berr, DSLM1 Time, DSLM1 berr, DSAM Time, DSAM berr'
-ALLOCATE(time(itmax, 3), backward_error(itmax, 3))
+WRITE(1,'(A)') 'Degree, DSLM Time, DSLM berr, DSLM1 Time, DSLM1 berr, DSLM2 Time, DSLM2 berr, DSAM Time, DSAM berr'
+ALLOCATE(time(itmax, 4), backward_error(itmax, 4))
 DO WHILE(deg<=maxDegree)
   WRITE(1,'(I10)', advance='no') deg
   WRITE(1,'(A)', advance='no') ','
@@ -57,7 +57,6 @@ DO WHILE(deg<=maxDegree)
     time(it, 1)=(dble(clock_stop-clock_start)/dble(clock_rate))
     backward_error(it, 1) = maxval(berr)
     DEALLOCATE(er, ei, berr)
-    PRINT*, 'DSLM:', deg
 
     !DSLM1 
     ALLOCATE(er(deg), ei(deg), berr(deg))
@@ -68,7 +67,16 @@ DO WHILE(deg<=maxDegree)
     time(it, 2)=(dble(clock_stop-clock_start)/dble(clock_rate))
     backward_error(it, 2) = maxval(berr)
     DEALLOCATE(er, ei, berr)
-    PRINT*, 'DSLM1:', deg  
+
+    !DSLM2
+    ALLOCATE(er(deg), ei(deg), berr(deg))
+    CALL system_clock(count_rate=clock_rate)
+    CALL system_clock(count=clock_start)
+    CALL dslm2(p, deg, er, ei, berr)
+    CALL system_clock(count=clock_stop)
+    time(it, 3)=(dble(clock_stop-clock_start)/dble(clock_rate))
+    backward_error(it, 3) = maxval(berr)
+    DEALLOCATE(er, ei, berr)
    
     !DSAM 
     ALLOCATE(er(deg), ei(deg), berr(deg))
@@ -76,10 +84,9 @@ DO WHILE(deg<=maxDegree)
     CALL system_clock(count=clock_start)
     CALL dsam(p, deg, er, ei, berr)
     CALL system_clock(count=clock_stop)
-    time(it, 3)=(dble(clock_stop-clock_start)/dble(clock_rate))
-    backward_error(it, 3) = maxval(berr)
+    time(it, 4)=(dble(clock_stop-clock_start)/dble(clock_rate))
+    backward_error(it, 4) = maxval(berr)
     DEALLOCATE(er, ei, berr)
-    PRINT*, 'DSAM:', deg
 
     DEALLOCATE(p, alpha)
 
@@ -96,7 +103,11 @@ DO WHILE(deg<=maxDegree)
   WRITE(1,'(A)', advance='no') ','
   WRITE(1,'(ES15.2)', advance='no') sum(time(:,3))/itmax
   WRITE(1,'(A)', advance='no') ','
-  WRITE(1,'(ES15.2)', advance='no') sum(backward_error(:,3))/itmax
+  WRITE(1,'(ES15.2)', advance='no') sum(backward_error(:,3))/itmax 
+  WRITE(1,'(A)', advance='no') ','
+  WRITE(1,'(ES15.2)', advance='no') sum(time(:,4))/itmax
+  WRITE(1,'(A)', advance='no') ','
+  WRITE(1,'(ES15.2)', advance='no') sum(backward_error(:,4))/itmax
   WRITE(1,'(A)')
   deg=2*deg
 ENDDO

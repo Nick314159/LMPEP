308027cf4b7ee5e166aa811f757bc50d578f06fd
Nick314159 <nsteckley314@gmail.com>
2017-09-30 15:48:44 -0700

    updated installer

--- develop/research/modification_test/install.sh
+++ develop/research/modification_test/install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-#compile test program to executable file
-gfortran -O3 test.f90 dslm1.f90 dslm2.f90 dslcorr1.f90 dzslcorr1.f90 dslcorr2.f90 dzslcorr2.f90 dsam.f90 dsacorr.f90 dzsacorr.f90 -llmpeptest -llmpep -o test.exe 
 
-gfortran -O3 poly_test.f90 dslm1.f90 dslm2.f90 dslcorr1.f90 dzslcorr1.f90 dslcorr2.f90 dzslcorr2.f90 dsam.f90 dsacorr.f90 dzsacorr.f90 -llmpeptest -llmpep -o poly_test.exe 
+gfortran -c -O3 src/*
 
+ar crv libmodtest.a *.o
+sudo cp libmodtest.a /usr/local/lib
 
+gfortran -O3 test.f90 -L/usr/local/lib -lmodtest -llmpep -o test.exe
+gfortran -O3 poly_test.f90 -L/usr/local/lib -lmodtest -llmpep -o poly_test.exe
+
+rm *.o
+rm *.a

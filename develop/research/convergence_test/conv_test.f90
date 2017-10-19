f36bd005097d8bc763418f62374ba46e579189bd
Nick314159 <nsteckley314@gmail.com>
2017-10-18 19:18:02 -0700

    update

--- develop/research/convergence_test/conv_test.f90
+++ develop/research/convergence_test/conv_test.f90
@@ -1,10 +1,10 @@
 PROGRAM conv_test
 IMPLICIT NONE
-INTEGER                                         :: deg, itmax
+INTEGER                                         :: deg, itmax, test_num
 DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE     :: p, berr, er, ei, error
 DOUBLE COMPLEX, DIMENSION(:), ALLOCATABLE       :: exacteigs
 !loop variables
-INTEGER                                         :: i
+INTEGER                                         :: i, j
 !intrinsic subroutines
 INTRINSIC                                       :: dble, dcmplx, dimag, epsilon
 !external subroutines
@@ -13,59 +13,41 @@ EXTERNAL                                        :: dslm_conv, dsam_conv, dsam
 CHARACTER(LEN=100)                              :: arg
 
 itmax = 60
+test_num = 127 
 
 CALL getarg(1,arg)  
 READ(arg, *) deg
 
+ OPEN(UNIT=1,FILE="results/dslm_conv_test_results.txt")
+DO j=1,test_num
+  !Create random polynomial
+  ALLOCATE(p(deg+1))
+  CALL daruv(deg+1,p)
 
-!Create random polynomial
-ALLOCATE(p(deg+1))
-CALL daruv(deg+1,p)
-!--------------------------------------
+  !DSLM 
+  ALLOCATE(berr(deg),er(deg),ei(deg), exacteigs(deg), error(itmax+1))
 
-!DSLM 
-OPEN(UNIT=1,FILE="results/dslm_conv_test_results.txt")
-ALLOCATE(berr(deg),er(deg),ei(deg), exacteigs(deg), error(itmax+1))
+  !Compute convergent vector
+  CALL dslm(p, deg, er, ei, berr)
+  DO i=1,deg
+    exacteigs(i) = dcmplx(er(i), ei(i))
+  ENDDO
+  DEALLOCATE(berr,er,ei)
 
-!Compute convergent vector
-CALL dslm(p, deg, er, ei, berr)
-DO i=1,deg
-  exacteigs(i) = dcmplx(er(i), ei(i))
+  !Test for convergence rate
+  ALLOCATE(berr(deg),er(deg),ei(deg))
+  CALL dslm_conv(p, deg, er, ei, berr, exacteigs, error)
+  DO i =1,itmax
+    WRITE(1, '(ES15.2E3)') error(i)  
+  ENDDO
+  DEALLOCATE(berr,er,ei, exacteigs, error, p)
+
+  WRITE(1,*)
+
 ENDDO
-DEALLOCATE(berr,er,ei)
-
-!Test for convergence rate
-ALLOCATE(berr(deg),er(deg),ei(deg))
-CALL dslm_conv(p, deg, er, ei, berr, exacteigs, error)
-DO i =1,itmax
-  WRITE(1, '(ES15.2E3)') error(i)  
-ENDDO
-DEALLOCATE(berr,er,ei, exacteigs, error)
 
  CLOSE(UNIT=1)
-!--------------------------------------
-
-!DSAM 
-OPEN(UNIT=1,FILE="results/dsam_conv_test_results.txt")
-ALLOCATE(berr(deg),er(deg),ei(deg), exacteigs(deg), error(itmax+1))
-
-!Compute convergent vector
-CALL dsam(p, deg, er, ei, berr)
-DO i=1,deg
-  exacteigs(i) = dcmplx(er(i), ei(i))
-ENDDO
-DEALLOCATE(berr,er,ei)
-
-!Test for convergence rate
-ALLOCATE(berr(deg),er(deg),ei(deg))
-CALL dsam_conv(p, deg, er, ei, berr, exacteigs, error)
-DO i =1,itmax
-  WRITE(1, '(ES15.2E3)') error(i)  
-ENDDO
-DEALLOCATE(berr,er,ei,p, exacteigs, error)
-
- CLOSE(UNIT=1)
-
 
 CALL EXECUTE_COMMAND_LINE('python conv_test.py')
+
 END PROGRAM conv_test
--- develop/research/convergence_test/conv_test.py
+++ develop/research/convergence_test/conv_test.py
@@ -1,30 +1,38 @@
 import csv
 import numpy as np
 import sys
+import random
 from matplotlib import pyplot as plt
-methods = ["dslm", "dsam"]
+from matplotlib import colors 
+methods = ["dslm"]
 
-for method in methods:
-  with open('results/'+method+'_conv_test_results.txt') as f:
-    reader = csv.reader(f)
-    error= []
-    for row in reader:
-       error.append(float(row[0].strip()))   
+with open('results/dslm_conv_test_results.txt') as f:
+  reader = csv.reader(f)
+  error= []
+  i = 0;
+  temp = []
+  for row in reader:
+     if len(row)>0:
+       temp.append(float(row[0].strip()))   
+     else:
+       i+=1
+       error.append(temp)
+       temp=[]
     
-    error=np.array(error)
-    error = [elem for elem in error if elem > sys.float_info.epsilon]
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(range(1, len(error)+1), error, 'k.', label='Error')
-    plt.subplots_adjust(bottom=.1, left=.16)
-    ax.set_yscale('log')
-    ax.set_xscale('log')
-    ax.set_xlabel('Iteration')
-    ax.set_ylabel('Error')
-    plt.title("Convergence Test "+method )
-    legend = ax.legend(loc=0, shadow=True)
-    plt.savefig("diagrams/"+method+"_conv_test_.pdf", format='pdf')
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  for j in range(i):
+    temp=np.array(error[j])
+    temp = [elem for elem in temp if elem > sys.float_info.epsilon]
+    ax.scatter(range(1, len(temp)+1), temp, marker=".", color="#%06x" % random.randint(0, 0xFFFFFF))
+  plt.subplots_adjust(bottom=.1, left=.16)
+  ax.set_yscale('log')
+  #ax.set_xscale('log')
+  ax.set_xlabel('Iteration')
+  ax.set_ylabel('Error')
+  plt.title("Convergence Test DSLM" )
+  legend = ax.legend(loc=0, shadow=True)
+  plt.savefig("diagrams/dslm_conv_test_.pdf", format='pdf')
 
     #x = error[1:len(error)-1]
     #y = error[2:len(error)]

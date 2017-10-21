b137da407a8b6802b529a8e7a6543cd0c666b4db
Thomas Cameron <thcameron@thomass-air.employeewireless.davidson.edu>
2017-10-21 18:34:21 -0400

    started testConvergence in tests folder

---
+++ develop/tests/testConvergence.f90
@@ -0,0 +1,23 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!   Thomas R. Cameron* and Nikolas I. Steckley#
+!                                                               
+!   *Dept. Mathematics and Computer Science, Davidson College
+!   #Dept. DiscoverOrg LLC.
+!       
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!   Last modified 21 October 2017
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!   Test Convergence on several famous polynomials and one random
+!   polynomial of degree 100. 
+!
+!   The root approximations are computed using DSLM and then
+!   an error vector is computed using DSLM_CONV.
+!   
+!   The error vector for all polynomials is written to a the
+!   file: "".
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+PROGRAM testConvergence
+IMPLICIT NONE
+
+
+END PROGRAM testConvergence
